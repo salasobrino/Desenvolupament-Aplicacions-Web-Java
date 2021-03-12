7934764430fd3fad540cfe281a4d9ce0e8ad83db
salasobrino <joanmariasalagodia@gmail.com>
2021-03-12 20:38:06 +0100

    five

--- Java1_1/src/Java1_Pack/Java_1_2.java
+++ Java1_1/src/Java1_Pack/Java_1_2.java
@@ -1,21 +1,46 @@
 package Java1_Pack;
 
+import java.util.Scanner;
+
 public class Java_1_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//prueba calculadora
+		Scanner sc = new Scanner(System.in);
 		
-		while(true){
+		System.out.println("Tecleja dos números: \n");
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		sum(x,y);	
+		rest(x,y);
+		
+	}
 			
-			boolean t= false;
 			
+		public static void sum (int x, int y) {
+			
+			int suma=x+y;
+			
+			System.out.println("El resultat de la suma és: " + suma);
 			
 			
 		}
+		
+		public static void rest (double x, double y) {
+			
+			double resta=x-y;
+			
+			System.out.println("El resultat de la resta és: " + resta);
+			
+			
+		}
+	
 	}
 
-	}
+	
 
 
