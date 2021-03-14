495c979b45c08709c00b15c2024c614f03853bfb
salasobrino <joanmariasalagodia@gmail.com>
2021-03-14 20:10:50 +0100

    eight branch 3

--- Java1_1/src/Java1_Pack/Java_1_2.java
+++ Java1_1/src/Java1_Pack/Java_1_2.java
@@ -1,60 +1,73 @@
 package Java1_Pack;
 
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Java_1_2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		// TODO Auto-generated method stub
 		
 		boolean terminar=false;
-		boolean quite=false;
-		
+				
 		//prueba calculadora
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 		
-		while(terminar == false) {
+		//Ingrese datos usando BufferReader
+        BufferedReader reader = 
+                   new BufferedReader(new InputStreamReader(System.in));
+		
+		while(terminar==false) {
 			
 		System.out.println("Tecleja dos números pel teclat: \n");
 					
-		int x= sc.nextInt();
-		int y = sc.nextInt();
-		
+		//int x= sc.nextInt();
+		//int y = sc.nextInt();
+		int x = Integer.parseInt(reader.readLine());
+		int y = Integer.parseInt(reader.readLine());
 					
-		System.out.println("Quina operació preferèixes (tecleja la inicial): suma (s), resta (r), divisió (d) o multiplicació (m), totes (t) o surt(quite)? \n");
-		char select = sc.next().charAt(0);
-		String sortir = sc.nextLine();
+		System.out.println("Quina operació preferèixes (tecleja la inicial): suma (s), resta (r), divisió (d) o multiplicació (m), totes (t) o surt(q)? \n");
+		String select = reader.readLine();
+		int numChar = select.length();	
 		
-		if(select == 's') {
-			sum(x,y);
-			terminar = true;
+		if (numChar>1) {
+			countChar(select);
+			terminar=true;
 			
-		} else if (select == 'r'){
+		}
+					
+		if(select == "s") {
+			sum(x,y);
+			terminar= true;
+			
+		} else if (select == "r"){
 			rest(x,y);
 			terminar = true;
 			
-		} else if (select == 'd'){
+		} else if (select == "d"){
 			div(x,y);
 			terminar = true;
 			
-		} else if (select == 'm'){
+		} else if (select == "m"){
 			mult(x,y);
 			terminar = true;
 			
-		} else if (select == 't'){
+		} else if (select == "t"){
 			sum(x,y);	
 			rest(x,y);
 			div(x,y);
 			mult(x,y);
 			terminar = true;
 			
-		} else if (sortir.equals(quite)) {
+		} else if (select == "q") {
 			
-			goOut(quite);
+			goOut();		
 			terminar = true;
 		}
 		
-		if(select!='s' && select!='r' && select!='d' & select!='m' && select!='t' && sortir!="quite") {
+		if(select!="s" && select!="r" && select!="d" & select!="m" && select!="t" && select!="q") {
 		
 			System.out.println("Error en la selecció de la inical. Torna a repertir-ho. \n");
 			
@@ -62,50 +75,61 @@ public class Java_1_2 {
 		
 		System.out.println("Operació finalitzada. \n");
 		
-		sc.close();
+		//sc.close();
+		//reader.close();
 		}
-		
+		}
 			
-	}
+	
+		public static boolean countChar(String numChar) {
 			
+				int numC = numChar.length();
+				System.out.println("Error. Torna a teclejar una sola lletra"+ numC);
+				return true;
+				
+			}
 			
-		public static void sum (int x, int y) {
+		public static boolean sum (int x, int y) {
 			
 			int suma = x+y;
 			
 			System.out.println("El resultat de la suma és: " + suma);
 			
+			return true;
 			
 		}
 		
-		public static void rest (double x, double y) {
+		public static boolean rest (double x, double y) {
 			
 			double resta = x-y;
 			
 			System.out.println("El resultat de la resta és: " + resta);
-			
+			return true;
 			
 		}
 		
-		public static void div (double x, double y) {
+		public static boolean div (double x, double y) {
 			
 			double division = x/y;
 			
 			System.out.println("El resultat de la divisió és: " + division);
+			return true;
 		}
 		
-		public static void mult (int x, int y) {
+		public static boolean mult (int x, int y) {
 			
 			int multi = x*y;
 			
 			System.out.println("El resultat de la divisió és: " + multi);
+			return true;
 			
 		}
 		
-		public static void goOut(boolean quite) {
+		public static boolean goOut() {
 			
-			quite = true;
+			
 			System.out.println("Usuari decideix sortir. \n");
+			return true;
 		}
 	
 	}
